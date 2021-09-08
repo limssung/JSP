f57dbabc677a9205f0aa448e3acc4881f5f50e85
limssung <soemen@naver.com>
2021-09-08 15:42:32 +0900

    add project JSP_XXXX

---
+++ JSP_git/src/com/java/main/Main.java
@@ -0,0 +1,11 @@
+package com.java.main;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("main");
+		System.out.println("java");
+		System.out.println("hellooooooooo");
+		System.out.println("goodBye");
+	}
+}
+
